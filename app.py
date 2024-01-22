973e2d38dd0f55eb9035ed016f4cfa7ff4359b4c
Rikin Zala <76729304+RikinZala25@users.noreply.github.com>
2024-01-22 13:57:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+# Configured route to /message
 @app.route('/message')
 def get_data():
     data = "Hello, World from Rikin Zala!"
