03393123184dec5858abc383e0c09d0f43cc7bc2
PuceHunter <151709727+PuceHunter@users.noreply.github.com>
2024-01-22 13:38:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-def app(environ, start_response):
-    data = b"Hello, World from Rikin Zala!\n"
-    start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(data)))
-    ])
-    return iter([data])
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/message')
+def get_data():
+    data = "Hello, World from Rikin Zala!"
+    response_data = {"message": data}
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run()
+
+# def app(environ, start_response):
+#     data = b"Hello, World from Rikin Zala!\n"
+#     start_response("200 OK", [
+#         ("Content-Type", "text/plain"),
+#         ("Content-Length", str(len(data)))
+#     ])
+#     return iter([data])
